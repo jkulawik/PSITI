b2629430969a05fbf3ba031419bbecf91ef7e49e
jkulawik <darkfire.9251@gmail.com>
2021-04-15 15:25:51 +0200

    Zadanie 1 chyba zrobione

--- lab1/zadanie1.m
+++ lab1/zadanie1.m
@@ -1,13 +1,33 @@
-
 % Parametry wejœciowe
 A = 3;
 f = 2;
 fx = f*10; % [Hz]
 
 % Oœ pozioma (czas)
-t = 0:1e-4:1;
+t_step = 1e-3;
+fs = 1/t_step; % Czêst. próbkowania
+t = 0:t_step:2;
+
 % Sygna³ wejœciowy
 swe = A*sin(2*pi*fx*t);
+%plot(t, swe); % Podgl¹d sygna³u
 
-% Podgl¹d
-plot(t, swe);
+% Ustawienia spektrogramów
+L = 128*4; % D³ugoœæ okna, wp³ywa na rozdzielczoœæ i dok³adnoœæ
+overlap = 120; % Musi byæ mniejsze od L
+freq_res = 128*2; % Rozdzielczoœæ czêst.
+
+% Okno prostok¹tne
+figure(1);
+spectrogram( swe, rectwin(L), overlap, freq_res, fs);
+title('Okno prostok¹tne')
+
+% Okno Hanna
+figure(2);
+spectrogram( swe, hann(L), overlap, freq_res, fs);
+title('Okno Hanna')
+
+% Okno Hamminga
+figure(3);
+spectrogram( swe, hamming(L), overlap, freq_res, fs);
+title('Okno Hamminga')
