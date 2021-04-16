1d030f745366505ade5fd3d483af6b46961f82ca
jkulawik <darkfire.9251@gmail.com>
2021-04-16 18:08:19 +0200

    3.3 fixes

--- lab1/zadanie3.m
+++ lab1/zadanie3.m
@@ -7,6 +7,7 @@ fx2 = 2000; % [Hz]
 fs = 12000; % Czêst. próbkowania [Hz]
 t_step = 1/fs;
 t = 0:t_step:2;
+l = length(t);
 
 % Do up³yniêcia pierwszej sekundy do swe wpisywana jest fx1
 % Po niej, fx2
@@ -18,7 +19,11 @@ for i=1:1:l
     end
 end
 
-cwt(swe, 'amor', fs); % amor oznacza falkê Gabora
+% Granice osi f:
+fg1 = 500;
+fg2 = 5000;
+cwt(swe, 'amor', fs, 'FrequencyLimits',[fg1 fg2]); % amor oznacza falkê Gabora
+%cwt(swe, 'amor', fs);
 
 %cqt(swe, 'SamplingFrequency', fs);
 
