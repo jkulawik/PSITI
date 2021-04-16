c1557a392025984dbfb8b76fe2d1c9514974e14e
jkulawik <darkfire.9251@gmail.com>
2021-04-16 20:10:59 +0200

    3.2

--- lab1/zadanie2.m
+++ lab1/zadanie2.m
@@ -1,3 +1,4 @@
+clear;
 % CWT = continuous wavelet transform
 
 % Parametry wejœciowe
@@ -13,11 +14,12 @@ t = 0:t_step:2;
 % UWAGA - zadanie niedokoñczone, niewiadomo o co chodzi
 
 n = 3; % Liczba realizacji
+swe = A*sin(2*pi*fx*t);
 
 for i=1:1:n
     swe = swe + A*sin(2*pi*fx*t + rand()*2*pi);
 end
-swe = swe/n;
-plot(t, swe)
+swe = swe/(n+1);
+%plot(t, swe);
 
-%cwt(swe, fs)
+cwt(swe, fs);
