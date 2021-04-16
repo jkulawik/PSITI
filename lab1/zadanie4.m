08fbfae572f153f977da62931c2e51dec8b2045a
Szkutnik Marek (STUD) <01133493@pw.edu.pl>
2021-04-16 20:00:12 +0200

    zad4

---
+++ lab1/zadanie4.m
@@ -0,0 +1,25 @@
+load handel.mat
+
+[y, Fs]=audioread('zad4.wav'); % Read wave file
+%sound(y,Fs);
+%info = audioinfo('zad4.wav');
+
+t = 0:seconds(1/Fs):seconds(info.Duration);
+t = t(1:end-1);
+fs = 1000; % Częst. próbkowania
+
+%figure(1);
+%plot(t,y);
+%xlabel('Time');
+%ylabel('Audio Signal');
+
+
+left=y(:,1); % Left channel
+right=y(:,2); % Right channel
+figure(1);
+plot((1:length(left))/Fs, left);
+%figure(2);
+%plot((1:length(right))/Fs, right);  
+figure(3);
+cqt(left)
+
